db5a1a3db2c94ae653212909fd05f9cf01bb93f5
GitHub Action <action@github.com>
2022-08-05 09:06:53 +0000

    Template cleanup

--- src/main/kotlin/com/github/vastemonde/scheduler/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/vastemonde/scheduler/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.vastemonde.scheduler.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.vastemonde.scheduler.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/vastemonde/scheduler/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/vastemonde/scheduler/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.vastemonde.scheduler.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.vastemonde.scheduler.MyBundle
 
 class MyApplicationService {
 
